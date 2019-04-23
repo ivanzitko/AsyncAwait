510c2e79ef661c2fb27e4782372da63391a0a566
Ivan Zitko <ivanzitko@gmail.com>
2019-04-23 10:37:57 +0200

    Modifikovan poslednji primer

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AsyncAwait
 {
@@ -6,7 +7,43 @@ namespace AsyncAwait
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            callMethod();
+            Console.ReadKey();
+        }
+
+        public static async void callMethod()
+        {
+            Task<int> task = Method1();
+            Method2();
+            int count = await task;
+            Method3(count);
+        }
+
+        public static async Task<int> Method1()
+        {
+            int count = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1 - " + i);
+                    count += 1;
+                }
+            });
+            return count;
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2 - " + i);
+            }
+        }
+
+        public static void Method3(int count)
+        {
+            Console.WriteLine("Total count is " + count);
         }
     }
 }
---
+++ MsExampleAsyncCall/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MsExampleAsyncCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            callMethod();
+
+            Console.ReadKey();
+        }
+
+        public static async void callMethod()
+        {
+            Coffee cup = PourCoffee();
+            Console.WriteLine("-------------------------- Coffee is SERVED");
+            var eggsTask = FryEggsAsync(5);
+            var baconTask = FryBaconAsync(2);
+            var toastTask = makeToastWithButterAndJamAsync(2);
+
+            var allTasks = new List<Task> { eggsTask, baconTask, toastTask };
+            while (allTasks.Any())
+            {
+                Task finished = await Task.WhenAny(allTasks);
+                if (finished == eggsTask)
+                {
+                    Console.WriteLine("-------------------------- Eggs are SERVED");
+                    allTasks.Remove(eggsTask);
+                    await eggsTask;
+                }
+                else if (finished == baconTask)
+                {
+                    Console.WriteLine("-------------------------- Bacon is SERVED");
+                    allTasks.Remove(baconTask);
+                    await baconTask;
+                }
+                else if (finished == toastTask)
+                {
+                    Console.WriteLine("-------------------------- Toast is SERVED");
+                    allTasks.Remove(toastTask);
+                    var toast = await toastTask;
+                }
+                else
+                    allTasks.Remove(finished);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("------------ Breakfast is SERVED! --------------");
+
+            async Task<Toast> makeToastWithButterAndJamAsync(int number)
+            {
+                var plainToast = await ToastBreadAsync(number);
+                ApplyButter(plainToast);
+                ApplyJam(plainToast);
+                return plainToast;
+            }
+        }
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("Coffe is poured!");
+            return new Coffee();
+        }
+
+        private static Toast ApplyButter(Toast plainToast)
+        {
+            Console.WriteLine("Butter is applied to toast!");
+            return plainToast;
+        }
+
+        private static Toast ApplyJam(Toast plainToast)
+        {
+            Console.WriteLine("Jam is applied to toast!");
+            return plainToast;
+        }
+
+        private static async Task<Toast> ToastBreadAsync(int number)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine("Toasts finished:" + i + "/" + number);
+                    Thread.Sleep(1000);
+                }
+            });
+            Console.WriteLine("Toast is toasted!");
+            return new Toast();
+        }
+
+        private static async Task FryEggsAsync(int number)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine("Eggs finished:" + i + "/" + number);
+                    Thread.Sleep(1000);
+                }
+            });
+            Console.WriteLine("Eggs are fried!");
+        }
+
+        private static async Task FryBaconAsync(int number)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine("Bacons finished:" + i + "/" + number);
+                    Thread.Sleep(1000);
+                }
+            });
+            Console.WriteLine("Bacon is fried!");
+        }
+
+        private class Toast
+        {
+        }
+
+        private class Coffee
+        {
+        }
+    }
+}
